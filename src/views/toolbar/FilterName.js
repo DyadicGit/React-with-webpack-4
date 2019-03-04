c06f4318c7ec601885e424601559d987cd1d3890
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-03-04 19:21:21 +0200

    now update inputComponent with filter functionalities

--- src/views/toolbar/FilterName.js
+++ src/views/toolbar/FilterName.js
@@ -4,9 +4,22 @@ import styled from 'styled-components';
 
 const FilterName = styled.div`
   grid-area: filterName;
+  place-content: center;
+  display: flex;
+  place-items: center;
 `;
+const RoundInput = styled.input`
+  width: 80%;
+  height: 40%;
+  border-radius: 15px;
+  font-size: larger;
+  padding-left: 5%;
+`;
+
 const View = ({ className }) => (
-  <FilterName className={className}></FilterName>
+  <FilterName className={className}>
+    <RoundInput type="text" placeholder="Name" />
+  </FilterName>
 );
 
 View.propTypes = {
