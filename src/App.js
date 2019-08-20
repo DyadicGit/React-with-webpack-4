647b13517e4650b2f71b8170c0202f6ca9d51f41
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-08-20 10:17:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import FetchButton from './views/fetchButton';
 
 const App = () => (
   <div>
-    <p>React here!</p>
-    <FetchButton />
+  <LandingPage />
   </div>
 );
 
---
+++ src/views/components/Booklet/index.js
@@ -0,0 +1,1 @@
+import {BookletComponent} from './BookletComponent';
