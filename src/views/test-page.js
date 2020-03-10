29b8af488fc8bba0c09bfc8e4707e3cf172d0a41
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2020-03-10 11:56:31 +0000

    FunctionComponent vs FunctionBasic

--- src/views/test-page.js
+++ src/views/test-page.js
@@ -1,19 +1,47 @@
-/** @jsx createElement */
-import { createElement } from 'react';
+import React, { useEffect, useState } from 'react';
 import './test-styles.css';
 
-const template = (
-  <article className="description-box">
-    <aside>some article goes here</aside>
-    <aside>another description</aside>
-  </article>
-);
+const FunctionBasic = (cnt) => {
+  console.log('render FunctionBasic', cnt)
+  return (
+    <article className="description-box">
+      FunctionBasic
+    </article>
+  );
+};
+
+const FunctionComponent = ({ cnt, order = 0}) => {
+  console.log('render FunctionComponent order:'+order, cnt)
+  return (
+    <article className="description-box">
+      FunctionComponent
+    </article>
+  )
+};
+
+const FunctionCreateElement = (_ref) => {
+  const cnt = _ref.cnt;
+  console.log('render FunctionCreateElement ', cnt);
+  return React.createElement("article", { className: "description-box" }, "FunctionCreateElement");
+};
+
 const TestPage = () => {
-  console.log(template)
+  const [cnt, setCnt] = useState();
+  useEffect(() => {
+    const timerNr = setInterval(() => setCnt((cnt) => (cnt || 0) + 1), 1000)
+    const timeoutNr = setTimeout(clear, 5000)
+    function clear() { clearInterval(timerNr); clearTimeout(timeoutNr) }
+    return clear;
+  },[]);
+  const functionComponent = <FunctionComponent cnt={cnt} order='first' />
+  const functionCreateElement = React.createElement(FunctionCreateElement, {cnt: cnt}) ;
   return (
     <section>
-      <h1>Testing place</h1>
-      {template}
+      <h1>Testing place {cnt}</h1>
+      {functionComponent}
+      {functionCreateElement}
+      {FunctionComponent({cnt, order: 'second'})}
+      {FunctionBasic(cnt)}
     </section>
   );
 };
