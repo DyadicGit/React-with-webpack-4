a36fcbf8c43adcc513042a85391a662a3c99b1f6
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2020-03-12 13:28:52 +0000

    using basic functions useContext hook doesn't work, only as <TagComponent/>

--- src/views/test-jsx-sugar.js
+++ src/views/test-jsx-sugar.js
@@ -1,46 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import './test-styles.css';
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from './test-page';
 
 const FunctionBasic = (cnt) => {
-  console.log('render FunctionBasic', cnt)
+  console.log('render FunctionBasic', cnt);
   return (
-    <article className="description-box">
-      FunctionBasic
-    </article>
+    <article className="description-box">FunctionBasic</article>
   );
 };
-
-const FunctionComponent = ({ cnt, order = 0}) => {
-  console.log('render FunctionComponent order:'+order, cnt)
+const FunctionComponent = ({ cnt, order = 0 }) => {
+  console.log('render FunctionComponent order:' + order, cnt);
   return (
-    <article className="description-box">
-      FunctionComponent
-    </article>
+    <article className="description-box">FunctionComponent</article>
   )
 };
-
 const FunctionCreateElement = (_ref) => {
   const cnt = _ref.cnt;
   console.log('render FunctionCreateElement ', cnt);
   return React.createElement("article", { className: "description-box" }, "FunctionCreateElement");
 };
 
+
 const TestJsxSyntaxSugar = () => {
-  const [cnt, setCnt] = useState();
+  const [cnt, setCnt] = useState(0);
+  const theme = useContext(ThemeContext);
+
   useEffect(() => {
-    const timerNr = setInterval(() => setCnt((cnt) => (cnt || 0) + 1), 1000)
-    const timeoutNr = setTimeout(clear, 5000)
-    function clear() { clearInterval(timerNr); clearTimeout(timeoutNr) }
+    const timerNr = setInterval(() => setCnt((cnt) => (cnt || 0) + 1), 1000);
+    const timeoutNr = setTimeout(clear, 3000);
+
+    function clear() {
+      clearInterval(timerNr);
+      clearTimeout(timeoutNr);
+    }
+
     return clear;
-  },[]);
-  const functionComponent = <FunctionComponent cnt={cnt} order='first' />
-  const functionCreateElement = React.createElement(FunctionCreateElement, {cnt: cnt}) ;
+  }, []);
+
+  const functionComponent = <FunctionComponent cnt={cnt} order='first' />;
+  const functionCreateElement = React.createElement(FunctionCreateElement, { cnt: cnt });
+
   return (
-    <section className="theme-blue">
+    <section className={theme || 'no-theme'}>
       <h1>Testing place {cnt}</h1>
       {functionComponent}
       {functionCreateElement}
-      {FunctionComponent({cnt, order: 'second'})}
+      {FunctionComponent({
+        cnt,
+        order: 'second'
+      })}
       {FunctionBasic(cnt)}
     </section>
   );
--- src/views/test-page.js
+++ src/views/test-page.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TestJsxSyntaxSugar } from './test-jsx-sugar';
+import './test-styles.css';
 
-const TestPage = () => {
-  return (
-    <TestJsxSyntaxSugar/>
-  )
+const themes = {
+  blue: 'theme-blue',
+  red: 'theme-red'
 };
 
-export { TestPage }
+export const ThemeContext = React.createContext(themes.blue);
+
+const useTheme = (initialState) => {
+  const [state, setState] = useState(initialState);
+  function dispatch(themeId) {
+    setState(themes[themeId]);
+  }
+  return [state, dispatch];
+};
+
+
+const ThemeSwitch = ({ theme, setTheme }) => {
+  const themeHandler = (e) => setTheme(e.target.value);
+  console.log('%c ThemeRadio',"background-color: black; color: red; font-weight: 900;");
+
+  return (
+    <div className={`${theme} radio`}>
+      <input type="radio" id="blue" name="theme" value="blue" onClick={themeHandler} />
+      <label htmlFor="blue">blue</label>
+      <input type="radio" id="red" name="theme" value="red" onClick={themeHandler} />
+      <label htmlFor="red">red</label>
+    </div>
+  );
+};
+
+const TestPage = () => {
+  const [theme, setTheme] = useTheme(themes.blue);
+  return (
+    <ThemeContext.Provider value={theme}>
+      {ThemeSwitch({theme, setTheme})}
+      <TestJsxSyntaxSugar />
+    </ThemeContext.Provider>
+  );
+};
+
+export { TestPage };
